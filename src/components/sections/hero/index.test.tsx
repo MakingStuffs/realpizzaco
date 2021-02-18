80ee64878f68e6dd8137ef94241451597b591e1f
Paul Singh <paulsingh@Pauls-MacBook-Pro.local>
2021-02-18 20:09:17 +0000

    - Hero element and a simple test to check that it renders to the page - Renders full screen image element with a gradient overlay and text - Style set in style.scss

---
+++ src/components/sections/hero/index.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import HeroSection from './index'
+
+describe('<HeroSection />', () => {
+  let element: HTMLElement
+
+  beforeEach(() => {
+    render(<HeroSection />)
+    const { container } = render(<HeroSection />)
+    if (container) {
+      element = container.querySelector('.hero-section') as HTMLElement
+    }
+  })
+
+  test('renders on page', () => {
+    expect(element).toBeInTheDocument()
+  })
+})
